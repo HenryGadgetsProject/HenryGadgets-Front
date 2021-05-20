d3cd45d7ac59c93b064f05f7935c3f1680725d2f
hollowblind095 <hollowblind095@gmail.com>
2021-05-20 09:57:32 -0300

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBarHome from '../Components/Organisms/NavBarHome'
 // import NavBar from '../Components/Organisms/NavBar'
 // import Breadcrumb from '../Components/Atoms/Breadcrumb'
@@ -15,12 +15,64 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'
 //import data from '../Data/categories'
 import { Link } from 'react-router-dom'
 
+import styled from 'styled-components'
+
+const NumbersContainer = styled.ul`
+    list-style: none;
+    display: flex;
+    align-self: center;
+    background: var(--pure-white);
+`
+const PageNumbers = styled.li`
+    font-size: 1.2em;
+    font-weight: 500;
+    text-align: center;
+    padding: 1.6em;
+    border: .1em solid var(--divider);
+    cursor: pointer;
+    transition: .5s;
+    &:hover {
+        background-color: #ff616f;
+    }
+    &.active {
+        font-weight: 700;
+        background-color: #ff1744;
+        color: black;
+    }
+`
+const Button = styled.button`
+    font-size: 1.2em;
+    text-align: center;
+    padding: 1.6em;
+    background-color: transparent;
+    border: .1em solid var(--divider);
+    color: black;
+    cursor: pointer;
+    transition: .5s;
+    &:hover {
+        background-color: #ff1744;
+    }
+    &:focus {
+        outline: none;
+    }
+`
+const LoadMoreButton = styled.button`
+    padding: 1rem;
+    background-color: #ff1744;
+    color: black;
+    font-size: 1.2em;
+    border: 1px solid black;
+`
+
 const Home = () => {
 
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(getProductsByCategoryName('todas'))
+        // ****** Paginate Data ******
+        // setData(products)
+        // ****** Paginate Data ******
     }, [dispatch, getProductsByCategoryName])
 
     const products = useSelector((state) => state.product.filteredProducts)
@@ -45,6 +97,81 @@ const Home = () => {
         // }
         // console.log('Tipo', typeof(toBoolean))
     }
+
+    // ******************** Paginado ********************
+
+    // Guardo la información en un estado
+    // const [data, setData] = useState([])
+
+    // Página actual, inicializada en 1
+    const [currentPage, setCurrentPage] = useState(1)
+    // Cards o Items que voy a mostrar por página
+    const [itemsPerPage, setItemsPerPage] = useState(12)
+
+    // Número de páginas que quiero mostrar
+    const [pageNumberLimit, setPageNumberLimit] = useState(5)
+    // Máximo de páginas
+    const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5)
+    // Mínimo de páginas
+    const [minPageNumberLimit, setMinPageNumberLimit] = useState(0)
+
+    // En cada página voy a insertar las cards
+    const pages = [];
+    for(let i = 1; i <= Math.ceil(products.length/itemsPerPage); i++) {
+        pages.push(i)
+    }
+
+    // Información de los items que voy a mostrar en cada página
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentItems = products.slice(indexOfFirstItem, indexOfLastItem)
+
+    const handleClick = (event) => {
+        setCurrentPage(Number(event.target.id))
+    }
+    const handleNextBtn = () => {
+        setCurrentPage(currentPage + 1)
+        if (currentPage + 1 > maxPageNumberLimit) {
+            setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit)
+            setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit)
+        }
+    }
+    const handlePrevBtn = () => {
+        setCurrentPage(currentPage - 1)
+        if ((currentPage - 1) % pageNumberLimit === 0) {
+            setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit)
+            setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit)
+        }
+    }
+    const handleMoreBtn = () => {
+        setItemsPerPage(itemsPerPage + 5)
+    }
+
+    let pageIncrementBtn = null
+    if (pages.length > maxPageNumberLimit) {
+        pageIncrementBtn = <Button onClick={handleNextBtn}>&hellip;</Button>
+    }
+
+    let pageDecrementBtn = null
+    if (minPageNumberLimit >= 1) {
+        pageDecrementBtn = <Button onClick={handlePrevBtn}>&hellip;</Button>
+    }
+
+    // Renderizamos los números de las páginas como (<Li>)
+    const renderPageNumbers = pages.map(number => {
+        if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+            return(
+                <PageNumbers 
+                className={currentPage === number ? 'active' : null} key={number} id={number} onClick={handleClick}>
+                    {number}
+                </PageNumbers>
+            ) 
+        } else {
+            return null;
+        }
+    })
+
+    // ******************** Paginado ********************
 
     return (
         <div className="container">
@@ -105,18 +232,30 @@ const Home = () => {
                 </aside>
 
                 <section>
-                    <div>
-                        {/* <Pagination /> */}
-                    </div>
-
                     <h2>Catálogo</h2>
 
                     <div className="popular-products">
                         {/* <TopServices - Cards /> */}
-                        <ProductCards products={products} />
 
+                        {/* <ProductCards products={products} /> */}
+                        <ProductCards products={currentItems} />
+                        
                         {/* Pasamos la parte lógica hacia ProductCards para ahorrar código en Home */}
                     </div>
+
+                    {/* <LoadMoreButton onClick={handleMoreBtn}>Cargar más productos</LoadMoreButton> */}
+
+                    <NumbersContainer>
+
+                        <Button onClick={handlePrevBtn} disabled={currentPage === pages[0] ? true : false}>Anterior</Button>                    
+                        {pageDecrementBtn}
+                        {renderPageNumbers}
+                        {pageIncrementBtn}
+                        <Button onClick={handleNextBtn} disabled={currentPage === pages[pages.length - 1] ? true : false}>Siguiente</Button>
+            
+                    </NumbersContainer>
+
+
                 </section>
                 {/* </div> */}
             </Main>
