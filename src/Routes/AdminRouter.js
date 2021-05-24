12fe7d054b871b8454cfb7104715749abbb28f40
ecampili <eduardo.campili@gmail.com>
2021-05-24 11:13:25 -0300

    se modifico page notFound para que sea generica

--- src/Routes/AdminRouter.js
+++ src/Routes/AdminRouter.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Admin from '../Pages/Admin'
+import NotFound from '../Pages/NotFound'
 
 const AdminRouter = () => {
     return (
         <div>
             <Switch>
                 <Route path='/' component={Admin} />
-
             </Switch>
         </div>
     )
